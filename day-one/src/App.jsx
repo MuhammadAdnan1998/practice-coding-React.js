d9bf4e2571b86be0ac61a6d0e0a2357c09c0c0f3
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-09-10 00:07:32 +0500

    Set up React.js project with Hello World

---
+++ day-one/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '60px',
+      backgroundColor: '#f0f0f0', // Light gray background
+    }}
+    >
+      <h1 style={{ color: '#ff6347' }}>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
